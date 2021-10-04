71fe8a4086e5a52777fb09a4afff989774f0eaf2
Jennie-geo <jennifer.isintume@decagon.dev>
2021-10-04 18:47:42 +0100

    second task completed

--- src/controllers/tasks_controller.ts
+++ src/controllers/tasks_controller.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { User } from "../models/user";
 import Task from "../models/task";
 import { cloudinaryUpload } from "../utils/cloudinary";
+import fileModel from "../models/file";
 
 import { Request, Response, NextFunction } from "express";
 interface userInterface extends Request {
@@ -34,78 +35,30 @@ export async function createTask(req: userInterface, res: Response) {
     res.status(400).send(err);
   }
 }
-export async function deleteTask(req: userInterface, res: Response) {
-  const taskId = await Task.findById({ _id: req.params.id });
-  if (!taskId) {
-    return res.status(404).send("No task with the id ${taskId} exists");
-  }
-  try {
-    await taskId.remove();
-    res.status(200).json({
-      msg: "Task with that id deleted from database successfully",
-      details: taskId,
-    });
-  } catch (err) {
-    res.status(400).json({ msg: err });
-  }
-}
-export async function getAllTasks(req: userInterface, res: Response) {
-  try {
-    const getTasks = await Task.find({});
-    console.log(getTasks);
-    if (getTasks.length < 1) {
-      return res.status(404).json({ msg: "No task created yet" });
-    }
-    res.status(200).json({ msg: getTasks });
-  } catch (err) {
-    res.status(400).send(err);
-  }
-}
 
 export async function uploadFileCloudinary(req: Request, res: Response) {
   const file = req.file;
   if (!req.file) {
-    return res.status(400).json({
-      msg: "no file was uploaded.",
-    });
+    return res.status(400).json({ msg: "no file was uploaded." });
   }
-
   const response = await cloudinaryUpload(
     file?.originalname as string,
     file?.buffer as Buffer
   );
   if (!response) {
-    return res.status(500).json({
-      msg: "Unable to upload file. please try again.",
-    });
+    return res
+      .status(500)
+      .json({ msg: "Unable to upload file. please try again." });
   }
-
   //data to keep
   const file_secure_url = response.secure_url;
-
   //done with processing.
-  res.json({
-    msg: "file uploaded successfully.",
-    fileUrl: file_secure_url,
+  const newUpload = new fileModel({
+    name: [file_secure_url],
   });
+  console.log(typeof newUpload);
+  await newUpload.save();
+  res
+    .status(200)
+    .json({ msg: "file uploaded successfully.", fileUrl: file_secure_url });
 }
-
-//   //define storagefor the images
-//   const storage = multer.diskStorage({
-//     //destination for files
-//     destination: (req: Request, file: any, cb: any) => {
-//       cb(null, "../public/files");
-//     },
-//     //add back the extension that was striped out by multer
-//     filename: (req: Request, file: any, cb: any) => {
-//       cb(null, Date.now() + file.originalname);
-//     },
-//   });
-//   //upload parameter for multer
-//   const upload = multer({
-//     storage: storage,
-//     limits: {
-//       fieldSize: 1024 * 1024 * 3, //3mb
-//     },
-//   });
-// }
