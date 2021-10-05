20202e64724498b83f0a46b5a72a2cf94d3e208c
Kayode Odole <odoleemma@gmail.com>
2021-10-05 23:49:43 +0100

    modified updateProject route

--- src/routers/project_router.ts
+++ src/routers/project_router.ts
@@ -1,14 +1,17 @@
-
-import {createInvite,createProject, updateProject, getAllProject} from '../controllers/projectController'
+import {
+  createInvite,
+  createProject,
+  updateProject,
+  getAllProject,
+} from "../controllers/projectController";
 import { Router } from "express";
 
 import { authorization } from "../authentication/Auth";
 const router = Router();
 
-router.post('/invite',authorization, createInvite);
-router.post('/project',authorization, createProject)
-router.put('/updateproject',authorization,updateProject)
-router.get('/getproject',authorization, getAllProject)
-
+router.post("/invite", authorization, createInvite);
+router.post("/project", authorization, createProject);
+router.put("/updateproject/:projectId", authorization, updateProject);
+router.get("/getproject", authorization, getAllProject);
 
 export default router;
