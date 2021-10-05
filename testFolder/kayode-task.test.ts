2e1b1c0cc53f814dd7daa78af0942cad63ae0e86
Kayode Odole <odoleemma@gmail.com>
2021-10-05 12:08:26 +0100

    writing test for getTask

--- testFolder/kayode-task.test.ts
+++ testFolder/kayode-task.test.ts
@@ -1,5 +1,13 @@
-// import supertestSession from "supertest-session";
+import supertestSession from "supertest-session";
+import { describe, beforeAll } from "@jest/globals";
+import app from "../src/app";
 
-// let request;
+let request;
 
-// beforeAll(()=> {request = supertestSession()})
+beforeAll(() => {
+  request = supertestSession(app);
+});
+
+describe("USER TEST", () => {
+  test("should allow user to get tasks assigned to them.");
+});
